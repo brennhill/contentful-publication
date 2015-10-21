2c2b5090c1266679e3858e65509244ab5930f60d
Brenn <brenn.hill@omnigon.com>
2015-10-21 15:34:50 -0400

    Added migrate script

--- bin/fullSync.js
+++ bin/fullSync.js
@@ -3,6 +3,9 @@
 'use strict';
 
 var ContentSync = require('../lib/ContentSync'),
+    ContentClean = require('../lib/ContentClean'),
+    ContentPurge = require('../lib/ContentPurge'),
+    Migrate = require('../lib/Migrate'),
   ModelSync = require('../lib/ModelSync'),
   argv = require('minimist')(process.argv.slice(2)),
   logger = require('../lib/logger'),
@@ -24,9 +27,13 @@ if (!argv.c || argv.help || argv.h) {
 
 var config = JSON.parse(fs.readFileSync(argv.c)),
   contentSync = ContentSync.fromConfig(config),
+  contentClean = ContentClean.fromConfig(config),
+  contentPurge = ContentPurge.fromConfig(config),
+  migrate = Migrate.fromConfig(config),
   modelSync = new ModelSync(config);
 
 var type = argv.t ? argv.t : '';
+var contentType = argv.i ? argv.i : '';
 
 switch (type) {
   case 'model':
@@ -47,6 +54,40 @@ switch (type) {
       .catch(function (error) {
         logger.error('Synchronization error ', error);
       });
+    break;
+  case 'repair':
+    contentClean.run()
+        .then(function () {
+          logger.info('Content repair is over');
+        })
+        .catch(function (error) {
+          logger.error('Content repair error ', error);
+        });
+    break;
+  case 'migrate':
+      migrate.run(contentType).then(function(){
+        console.log("ran migrate");
+      })
+    break;
+  case 'purge':
+    contentPurge.run().then(function () {
+      logger.info('Purge is complete');
+    })
+    .catch(function (error) {
+      logger.error('Content purge error ', error);
+    });
+    break;
+  case 'forceCopy':
+      contentClean.run().then(function() {
+        return contentPurge.run().then(function(){
+          logger.info('Purge is complete. Beginning copy.');
+          return contentSync.run();
+        }, function(error){
+          logger.error('Content purge error ', error);
+        })
+      }).catch(function(error) {
+        logger.error('Content clean error ', error);
+      })
     break;
   default:
     modelSync.run()
